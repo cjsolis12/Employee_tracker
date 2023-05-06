21ee9dec7ac086817f57e22c79e941e28ab231a6
cjsolis <cjelizondo12@gmail.com>
2023-05-06 10:16:09 -0500

    changed the update an employee switch statement and made the inquirer into a function to export it

--- inquirerFile.js
+++ inquirerFile.js
@@ -1,103 +1,90 @@
+const inquirer = require('inquirer');
+const mysql = require("mysql2");
 
-const questions = [
-        {
-            type: "list",
-            name: "options",
-            message: "What would you like to do? (use arrow keys)",
-            choices:[
-              "View All Departments",
-              "View All Roles",
-              "View All Employees",
-              "Add a Department",
-              "Add a Role",
-              "Add an Employee",
-              "Update an Employee",
-              "Update an Employee Role",
-            ],
-        },
-      
-          {
-            type: "input",
-            name: "departmentName",
-            message: "Enter a new department name:",
-            when: (answers) => answers.options === "Add a Department",
-          },
-          {
-            type: "input",
-            name: "roleTitle",
-            message: "Add a new role title:",
-            when: (answers) => answers.options === "Add a Role",
-          },
-          {
-            type: "input",
-            name: "roleSalary",
-            message: "Enter a salary:",
-            when: (answers) => answers.options === "Add a Role",
-          },
-          {
-            type: "input",
-            name: "roleDepartment",
-            message: "Add a department ID for this role:",
-            when: (answers) => answers.options === "Add a Role",
-          },
-          {
-            type: "input",
-            name: "firstName",
-            message: "Enter the employee's first name:",
-            when: function (answers) {
-              return answers.options === "Add an Employee";
-            },
-          },
-          {
-            type: "input",
-            name: "lastName",
-            message: "Enter the employee's last name:",
-            when: function (answers) {
-              return answers.options === "Add an Employee";
-            },
-          },
-          {
-            type: "input",
-            name: "role",
-            message: "Enter the employee's role:",
-            when: function (answers) {
-              return answers.options === "Add an Employee";
-            },
-          },
-          {
-            type: "input",
-            name: "department",
-            message: "Enter the employee's department:",
-            when: function (answers) {
-              return answers.options === "Add an Employee";
-            },
-          },
-          {
-            type: "input",
-            name: "salary",
-            message: "Enter the employee's salary:",
-            when: function (answers) {
-              return answers.options === "Add an Employee";
-            },
-          },
-          {
-            type: "input",
-            name: "manager",
-            message: "Enter the employee's manager:",
-            when: function (answers) {
-              return answers.options === "Add an Employee";
-            },
-          },
-          {
-            type: "input",
-            name: "updateRole",
-            message: "Enter an updated role: :",
-            when: function (answers) {
-              return answers.options === "Update an Employee Role";
-            },
-          },
-    ]
 
+ async function init () {
+  return inquirer.prompt([
+    {
+        type: "list",
+        name: "options",
+        message: "What would you like to do? (use arrow keys)",
+        choices:[
+          "View All Departments",
+          "View All Roles",
+          "View All Employees",
+          "Add a Department",
+          "Add a Role",
+          "Add an Employee",
+          "Update an Employee Role",
+          "Exit"
+        ],
+    },
   
+      {
+        type: "input",
+        name: "departmentName",
+        message: "Enter a new department name:",
+        when: (answers) => answers.options === "Add a Department",
+      },
+      {
+        type: "input",
+        name: "roleTitle",
+        message: "Add a new role title:",
+        when: (answers) => answers.options === "Add a Role",
+      },
+      {
+        type: "input",
+        name: "roleSalary",
+        message: "Enter a salary:",
+        when: (answers) => answers.options === "Add a Role",
+      },
+      {
+        type: "input",
+        name: "roleDepartment",
+        message: "Add a department ID for this role:",
+        when: (answers) => answers.options === "Add a Role",
+      },
+      {
+        type: "input",
+        name: "firstName",
+        message: "Enter the employee's first name:",
+        when: function (answers) {
+          return answers.options === "Add an Employee";
+        },
+      },
+      {
+        type: "input",
+        name: "lastName",
+        message: "Enter the employee's last name:",
+        when: function (answers) {
+          return answers.options === "Add an Employee";
+        },
+      },
+      {
+        type: "input",
+        name: "role",
+        message: "Enter the employee's role ID:",
+        when: function (answers) {
+          return answers.options === "Add an Employee";
+        },
+      },
+      {
+        type: "input",
+        name: "department",
+        message: "Enter the employee's department ID:",
+        when: function (answers) {
+          return answers.options === "Add an Employee";
+        },
+      },
+      {
+        type: "input",
+        name: "manager",
+        message: "Enter the employee's manager ID:",
+        when: function (answers) {
+          return answers.options === "Add an Employee";
+        },
+      }
+    ])
+}
 
-module.exports = { questions };
+module.exports = init;
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const { questions } = require("./inquirerFile.js");
+const init = require("./inquirerFile.js");
 const mysql = require("mysql2");
 const inquirer = require("inquirer");
 
@@ -14,7 +14,9 @@ const dbConfig = {
 async function sqlConnection() {
   const connection = await mysql.createConnection(dbConfig);
   //Prompt the user for input
-  inquirer.prompt(questions).then((answers) => {
+
+  async function handleUserInput() {
+    const answers = await init();
     // Handle the user's input
     switch (answers.options) {
       case "View All Departments":
@@ -24,10 +26,13 @@ async function sqlConnection() {
         });
         break;
       case "View All Roles":
-        connection.query("SELECT role.id, role.title, role.salary, department.table_name AS department FROM role JOIN department ON role.department_id = department.id", function (err, results) {
-          if (err) throw err;
-          console.table(results);
-        });
+        connection.query(
+          "SELECT role.id, role.title, role.salary, department.table_name AS department FROM role JOIN department ON role.department_id = department.id",
+          function (err, results) {
+            if (err) throw err;
+            console.table(results);
+          }
+        );
         break;
       case "View All Employees":
         connection.query(
@@ -99,31 +104,120 @@ async function sqlConnection() {
         break;
       case "Add an Employee":
         connection.query(
-          "INSERT INTO employee (first_name, last_name, job_title, department, salary, managers) VALUES (?, ?, ?, ?, ?, ?)",
-          [
-            answers.firstName,
-            answers.lastName,
-            answers.role,
-            answers.department,
-            answers.salary,
-            answers.manager,
-          ]
+          "INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)",
+          [answers.firstName, answers.lastName, answers.role, answers.manager],
+          function (err, res) {
+            if (err) throw err;
+            connection.query(
+              "SELECT employee.id, employee.first_name, employee.last_name, role.title, table_name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON employee.manager_id = manager.id",
+              (err, res) => {
+                if (err) throw err;
+
+                // Display the updated table to the user
+                console.table(res);
+              }
+            );
+          }
         );
-        console.log("Employee  added successfully.");
         break;
-      case "Update an Employee":
-        // Perform the appropriate MySQL query to update the employee's information
-        console.log("Update an Employee");
-        break;
-      case "Update an Employee Role":
-        // Perform the appropriate MySQL query to update the employee's role
-        console.log("Update an Employee Role");
-        break;
+        case "Update an Employee Role":
+          // Query the database to get all employees
+          connection.query("SELECT * FROM employee", function (err, employeeResults) {
+            if (err) {
+              console.error(err);
+              return;
+            }
+        
+            // Map the employee records to an array of choices for the inquirer prompt
+            const employeeChoices = employeeResults.map((employee) => ({
+              name: `${employee.first_name} ${employee.last_name}`,
+              value: employee.id,
+            }));
+        
+            // Prompt the user to select the employee to update
+            inquirer
+              .prompt([
+                {
+                  type: "list",
+                  message: "Which employee do you want to update?",
+                  name: "employeeToUpdate",
+                  choices: employeeChoices,
+                },
+              ])
+              .then(function (answers) {
+                // Query the database to get all roles
+                connection.query("SELECT * FROM role", function (err, roleResults) {
+                  if (err) {
+                    console.error(err);
+                    return;
+                  }
+        
+                  // Map the role records to an array of choices for the inquirer prompt
+                  const roleChoices = roleResults.map((role) => ({
+                    name: role.title,
+                    value: role.id,
+                  }));
+        
+                  // Prompt the user to select the employee's new role
+                  inquirer
+                    .prompt([
+                      {
+                        type: "list",
+                        message: "What is the employee's new role?",
+                        name: "newRole",
+                        choices: roleChoices,
+                      },
+                    ])
+                    .then(function (roleAnswer) {
+                      // Perform the database update with the selected employee and new role
+                      connection.query(
+                        "UPDATE employee SET role_id = ? WHERE id = ?",
+                        [roleAnswer.newRole, answers.employeeToUpdate],
+                        function (err, updateResults) {
+                          if (err) {
+                            console.error(err);
+                            return;
+                          }
+                          console.log("Employee role updated successfully!");
+        
+                          // Display the updated employee table
+                          connection.query(
+                            "SELECT e.id, e.first_name, e.last_name, r.title AS role, d.table_name AS department, r.salary, m.first_name AS manager_first_name, m.last_name AS manager_last_name FROM employee e JOIN role r ON e.role_id = r.id JOIN department d ON r.department_id = d.id LEFT JOIN employee m ON e.manager_id = m.id",
+                            function (err, displayResults) {
+                              if (err) {
+                                console.error(err);
+                                return;
+                              }
+                              const tableData = displayResults.map((row) => {
+                                return {
+                                  id: row.id,
+                                  first_name: row.first_name,
+                                  last_name: row.last_name,
+                                  role: row.role,
+                                  department: row.department,
+                                  salary: row.salary,
+                                  managers: `${row.manager_first_name} ${row.manager_last_name}`,
+                                };
+                              });
+                              console.table(tableData);
+                              nextAction();
+                            }
+                          );
+                        }
+                      );
+                    });
+                });
+              });
+          });
+          break;
+        
       default:
         console.log("Invalid option selected.");
     }
-    console.log(answers);
-  });
+    await handleUserInput();
+  }
+
+  await handleUserInput();
 }
 
 sqlConnection();
